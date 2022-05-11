76ef6f8df5c4a33fb1593579ef298075499d271d
Alex <alexandr-makedonsky@yandex.ru>
2022-05-11 10:37:12 +0300

    add05.3PracticWork

---
+++ 05.3_iFraim/src/test/java/all/Computer/MainPage.java
@@ -0,0 +1,47 @@
+package all.Computer;
+import all.basePage.Page;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MainPage extends Page {
+
+    @FindBy(tagName = "iframe")
+    public WebElement iframeElement;
+    @FindBy(css = "input.email")
+    public WebElement emailInput;
+    @FindBy(id = "write-to-me")
+    public WebElement mainButton;
+    @FindBy(css = "pre[name='result-email']")
+    public WebElement emailResult;
+    @FindBy(className = "header")
+    public WebElement header;
+    @FindBy(css = "a.footer__contactItem.descriptionText")
+    public WebElement footerSkillboxLink;
+
+    private String url = "";
+
+    public MainPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void open() {
+        driver.navigate().to(url);
+    }
+
+    public void switchToFrame() {
+        driver.switchTo().frame(iframeElement);
+    }
+
+    public void switchToMainPage() {
+        driver.switchTo().defaultContent();
+    }
+
+    public void switchToParrentFrame() {
+        driver.switchTo().parentFrame();
+    }
+}
---
+++ 05.3_iFraim/src/test/java/all/Computer/Tests.java
@@ -0,0 +1,63 @@
+package all.Computer;
+import all.TestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Tests extends TestBase {
+
+    @Test
+    public void mainPage__sendEmail__success__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        //active
+        page.switchToFrame();
+        var email = "test@skillbox.com";
+        page.emailInput.sendKeys(email);
+        page.mainButton.click();
+        //assert
+        Assertions.assertEquals(email, page.emailResult.getText(), "Email не соответствует введённому!");
+    }
+
+    @Test
+    public void mainPage__sendEmail__headerIsVisible__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        //active
+        page.switchToFrame();
+        var email = "test@skillbox.com";
+        page.emailInput.sendKeys(email);
+        page.mainButton.click();
+        page.switchToMainPage();
+        //assert
+        Assertions.assertTrue(page.header.isDisplayed(), "Заголовок не отображается!");
+    }
+
+    @Test
+    public void mainPage__clickOnSkillboxLink__wasOpenedNewWindow__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        //active
+        page.switchToFrame();
+        page.switchToFrame();
+        page.footerSkillboxLink.click();
+        //assert
+        Assertions.assertEquals(2, getAllWindows().size(), "Не открылось новое окно!");
+    }
+
+    @Test
+    public void mainPage__clickOnSkillboxLink__emailFieldIsDisplayed__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        //active
+        page.switchToFrame();
+        page.switchToFrame();
+        page.footerSkillboxLink.click();
+        page.switchToParrentFrame();
+        //assert
+        Assertions.assertTrue(page.emailInput.isDisplayed(), "Не отображается поле для ввода email!");
+    }
+}
